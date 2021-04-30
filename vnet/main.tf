85574e1efc45ff32b5233046e07214447f4f2b33
Vaishu <vaisumathy@gmail.com>
2021-04-30 13:55:43 +0530

    do terraform plan

--- vnet/main.tf
+++ vnet/main.tf
@@ -1,9 +1,62 @@
-
+variable "location"{
+  type = string
+}
+variable "rgName"{
+  type = string
+}
+variable "client_id" {
+  type = string
+}
+variable "client_secret" {
+  type = string
+}
+variable "subscription_id" {
+  type = string
+}
+variable "tenant_id" {
+  type = string
+}
 terraform {
   backend "azurerm" {
     resource_group_name  = "32943"
     storage_account_name = "cloudlegonewstorage"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
+  }
+}
+provider "azurerm" {
+   client_id = var.client_id
+	 client_secret = var.client_secret
+	 subscription_id = var.subscription_id
+	 tenant_id = var.tenant_id
+	 
+	 features {}
+}
+resource "azurerm_virtual_network" "example" {
+  name                = "virtualNetwork1"
+  location            = var.location
+  resource_group_name = var.rgName
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+  }
+
+  subnet {
+    name           = "subnet3"
+    address_prefix = "10.0.3.0/24"
+    
+  }
+
+  tags = {
+    environment = "Production"
   }
 }
