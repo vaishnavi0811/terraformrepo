ce389bd4c62d31256ac2a6ded7a321023fb9d94a
Vaishu <vaisumathy@gmail.com>
2021-05-01 13:18:19 +0530

    moved creds to env vars

--- vnet/main.tf
+++ vnet/main.tf
@@ -25,8 +25,8 @@ resource "azurerm_virtual_network" "example" {
   name                = "virtualNetwork1"
   location            = var.location
   resource_group_name = var.rgName
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+  address_space       = ["10.0.1.0/16"]
+  dns_servers         = ["10.0.1.4", "10.0.1.5"]
 
   tags = {
     environment = "Production"
